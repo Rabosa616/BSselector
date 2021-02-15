4d8c39b2a510cdca1aa45103fb8768d1829a32f7
Marc GC <carnicer@hp.com>
2021-02-15 14:13:57 +0100

    improve generating script

--- project/examples/s.sh
+++ project/examples/s.sh
@@ -1,2 +1,7 @@
-pandoc -t dzslides -s slidesShort.md -o slidesShort.html
+DOC=slidesShort
+echo processing $DOC ...
+pandoc -t dzslides -s $DOC.md -o $DOC.html || exit 1
+DOC=${DOC}_comment
+echo processing $DOC ...
+pandoc -t dzslides -s $DOC.md -o $DOC.html || exit 1
 patch slidesShort.html < styleOK.patch
