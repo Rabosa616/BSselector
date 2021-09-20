794e138d80295f26e5cd1bcd6d24ebc7a6eab66b
Marc GC <carnicer@hp.com>
2021-09-20 17:22:58 +0200

    generate only twitch slides

--- project/examples/s.sh
+++ project/examples/s.sh
@@ -1,3 +1,10 @@
+DOC=twitch1
+echo processing $DOC ...
+pandoc -t dzslides -s $DOC.md -o $DOC.html || exit 1
+patch $DOC.html < styleOK.patch
+
+exit 0
+
 DOC=slidesShort
 echo processing $DOC ...
 pandoc -t dzslides -s $DOC.md -o $DOC.html || exit 1
