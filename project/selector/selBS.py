3feb982968f214ad8c8ddf5b1756ba6cc8e9af3d
marc <badukaire@gmail.com>
2021-09-28 21:03:34 +0200

    dict number of developers

--- project/selector/selBS.py
+++ project/selector/selBS.py
@@ -33,6 +33,98 @@ class BranchingStrategy :
   }
 
 
+  dictBS_1_developers = {
+    BS_NONE                 : True,
+    BS_SINGLEDEV            : True,
+    BS_ANARCHY              : True,
+    BS_TRUNKBASED           : True,
+    BS_ONEFLOWEXTENDED      : True,
+
+    BS_CONTINUOUSINTEG      : False,
+    BS_FORKINGFLOW          : False,
+  }
+
+  dictBS_1_3_developers = {
+    BS_NONE                 : True,
+    BS_SINGLEDEV            : True,
+    BS_ANARCHY              : True,
+    BS_TRUNKBASED           : True,
+    BS_GITHUBFLOW           : True,
+    BS_GITLABFLOW           : True,
+    BS_ONEFLOW              : True,
+    BS_CONTINUOUSINTEG      : True,
+    BS_SIMPLIFIEDGITFLOW1   : True,
+    BS_ONEFLOWEXTENDED      : True,
+
+    BS_FORKINGFLOW          : False,
+  }
+
+  dictBS_2_8_developers = {
+    BS_TRUNKBASED           : True,
+    BS_ONEFLOWEXTENDED      : True,
+    BS_CONTINUOUSINTEG      : True,
+    BS_GITFLOW              : True,
+    BS_GITHUBFLOW           : True,
+    BS_GITLABFLOW           : True,
+    BS_ONEFLOW              : True,
+    BS_SIMPLIFIEDGITFLOW1   : True,
+    BS_ONEFLOWEXTENDED      : True,
+    BS_FORKINGFLOW          : True,
+
+    BS_NONE                 : False,
+    BS_SINGLEDEV            : False,
+    BS_ANARCHY              : False,
+  }
+
+  dictBS_5_13_developers = {
+    BS_TRUNKBASED           : True,
+    BS_ONEFLOWEXTENDED      : True,
+    BS_GITFLOW              : True,
+    BS_GITLABFLOW           : True,
+    BS_ONEFLOW              : True,
+    BS_SIMPLIFIEDGITFLOW1   : True,
+    BS_ONEFLOWEXTENDED      : True,
+    BS_FORKINGFLOW          : True,
+
+    BS_NONE                 : False,
+    BS_SINGLEDEV            : False,
+    BS_ANARCHY              : False,
+  }
+
+  dictBS_8_21_developers = {
+    BS_ONEFLOWEXTENDED      : True,
+    BS_CONTINUOUSINTEG      : True,
+    BS_GITFLOW              : True,
+    BS_GITLABFLOW           : True,
+    BS_ONEFLOW              : True,
+    BS_SIMPLIFIEDGITFLOW1   : True,
+    BS_ONEFLOWEXTENDED      : True,
+    BS_FORKINGFLOW          : True,
+
+    BS_NONE                 : False,
+    BS_SINGLEDEV            : False,
+    BS_ANARCHY              : False,
+    BS_GITHUBFLOW           : False,
+    BS_CONTINUOUSINTEG      : False,
+  }
+
+  dictBS_21_more_developers = {
+    BS_ONEFLOWEXTENDED      : True,
+    BS_CONTINUOUSINTEG      : True,
+    BS_GITFLOW              : True,
+    BS_GITLABFLOW           : True,
+    BS_ONEFLOW              : True,
+    BS_SIMPLIFIEDGITFLOW1   : True,
+    BS_ONEFLOWEXTENDED      : True,
+    BS_FORKINGFLOW          : True,
+
+    BS_NONE                 : False,
+    BS_SINGLEDEV            : False,
+    BS_ANARCHY              : False,
+    BS_GITHUBFLOW           : False,
+    BS_CONTINUOUSINTEG      : False,
+  }
+
 
 if __name__ == "__main__":
 
